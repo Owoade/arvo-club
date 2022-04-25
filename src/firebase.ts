9e29f65bd4674b7b59448162f06069ee8c04e746
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-25 01:17:06 +0000

    fixed all UI

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,6 +10,14 @@ const firebaseConfig = {
     messagingSenderId: "938727130264",
     appId: "1:938727130264:web:205728a55c4cb3896ebced"
   };
+  // const firebaseConfig = {
+  //   apiKey: process.env.REACT_APP_FB_API_KEY,
+  //   authDomain:process.env.REACT_APP_FB_AUTH_DOMAIN ,
+  //   projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  //   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  //   messagingSenderId:process.env.REACT_APP_FB_SENDER_ID,
+  //   appId: process.env.REACT_APP_FB_APP_ID
+  // };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app);

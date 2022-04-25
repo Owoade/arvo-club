f1f4a1e66d08038efd8651ea125dcc58d486b329
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-25 02:22:42 +0000

    made the logo a link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
 import {Text} from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom";
+
 const Logo = ({alignCenter,dashboard}:{alignCenter?:boolean,dashboard?:true})=>{
-  
-  if(dashboard)  return <Text textAlign={alignCenter ? "center" : "left"} transform={{xs:"translateY(0)",base:"translateY(.6em)"}} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text> 
+  const navigate=useNavigate();
+  if(dashboard)  return <Text onClick={()=> navigate("/dashboard") } textAlign={alignCenter ? "center" : "left"} transform={{xs:"translateY(0)",base:"translateY(.6em)"}} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text> 
   return <Text textAlign={alignCenter ? "center" : "left"} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text>
 }
 export default Logo

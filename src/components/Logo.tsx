bddee12418b4494015acd323615659cb0a4cdda8
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-01 13:27:29 -0700

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Logo = ({alignCenter,dashboard}:{alignCenter?:boolean,dashboard?:true})=>{
   const navigate=useNavigate();
-  if(dashboard)  return <Text onClick={()=> navigate("/dashboard") } textAlign={alignCenter ? "center" : "left"} transform={{xs:"translateY(0)",base:"translateY(.6em)"}} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text> 
+  if(dashboard)  return <Text onClick={()=> navigate("/dashboard") } cursor="pointer" textAlign={alignCenter ? "center" : "left"} transform={{xs:"translateY(0)",base:"translateY(.6em)"}} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text> 
   return <Text textAlign={alignCenter ? "center" : "left"} fontFamily="Pacifico, cursive" color="brand.accent" fontSize={{sm:"30px",base:"20px"}}>ArvoClub<span style={{color:"#FFD809"}}>.</span> </Text>
 }
 export default Logo
